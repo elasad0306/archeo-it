852f5ba791b72260404fb5285786e2d65e07520f
El Asad <elasadbennordinehamissi@gmail.com>
2025-04-26 16:42:09 +0200

    Create navbar

--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 <html lang="fr">
     <head>
         <meta charset="utf8">
+        <title>Accueil</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/style.css">
     </head>
     <body>
         <header>
+            <div id="logo">
+                <span>Archeo-It</span>
+            </div>
+            <div id="navbar">
+                <nav>
+                    <a>Accueil</a>
+                    <a>Découvertes</a>
+                    <a>Contact</a>
+                </nav>
+            </div>
         </header>
         <p>Ceci est un test</p>
     </body>
