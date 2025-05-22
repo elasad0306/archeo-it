089f140f8ba0abcc68066ca53324df4d396a0594
El Asad <elasadbennordinehamissi@gmail.com>
2025-05-22 23:17:16 +0200

    Ajout du fichier header.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <div id="logo">
+        <span>Archeo-It</span>
+    </div>
+    <div id="navbar">
+        <nav>
+            <a href="index.php">Accueil</a>
+            <a href="discovery.php">Découvertes</a>
+            <a>Contact</a>
+        </nav>
+    </div>
+</header>
