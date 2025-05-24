1a3eddd361e357af5f45e60b1e818adc00befe21
El Asad <elasadbennordinehamissi@gmail.com>
2025-05-24 19:14:04 +0200

    Ajout des champs à remplir et ajout de style sur la page d'inscription

--- register.php
+++ register.php
@@ -1,62 +1,30 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="utf8">
-    <title>Accueil</title>
-    <script src="https://kit.fontawesome.com/0cb37bcd4e.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/home.css">
-    <link rel="stylesheet" href="assets/css/connection.css">
-</head>
+<?php include 'head.php'; ?>
 <body>
-<header>
-    <div id="logo">
-        <span>Archeo-It</span>
-    </div>
-    <div id="navbar">
-        <nav>
-            <a>Accueil</a>
-            <a>Découvertes</a>
-            <a>Contact</a>
-        </nav>
-    </div>
-</header>
 <section>
-    <div id="register_content">
-        <h1>Archeo-It</h1>
-        <form method="get">
-            <!--For email!-->
-            <p class="control has-icons-left has-icons-right">
-                <input class="input" type="email" placeholder="Email" id="mail"/>
-                <span class="icon is-small is-left">
-            <i class="fas fa-envelope"></i>
-        </span>
-                <span class="icon is-small is-right">
-            <i class="fas fa-check"></i>
-        </span>
-            </p>
-            <!--For username!-->
-            <p class="control has-icons-left has-icons-right">
-                <input class="input" type="text" placeholder="Pseudo" id="username"/>
-                <span class="icon is-small is-left">
-                <i class="fa-solid fa-user"></i>
-            </span>
-            </p>
-            <!--For password!-->
-            <p class="control has-icons-left">
-                <input class="input" type="password" placeholder="Mot de passe" id="password"/>
-                <span class="icon is-small is-left">
-                <i class="fas fa-lock"></i>
-            </span>
-            </p>
-            <!--Button sing in!-->
-            <input type="submit" class="button is-success" value="Se connecter"/>
+    <h1>Archeo-It</h1>
+    <div>
+        <span>Retour</span>
+        <!--Champ à remplir !-->
+        <form>
+            <!--Champ pour l'email !-->
+            <div class="fields">
+                <input type="email" placeholder="Email">
+            </div>
+            <!--Champ pour le pseudo !-->
+            <div class="fields">
+                <input type="text" placeholder="Pseudo">
+            </div>
+            <!--Champ pour le mot de passe !-->
+            <div class="fields">
+                <input type="password" placeholder="Mot de passe">
+            </div>
+            <!--- Boutton pour soumettre les informations !-->
+            <div class="fields">
+                <input type="submit" value="S'inscrire">
+            </div>
         </form>
-
     </div>
 </section>
 
