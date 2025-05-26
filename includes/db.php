7e1ffe41233b37fb6e0471e4a0929b986b96038a
El Asad <elasadbennordinehamissi@gmail.com>
2025-05-26 10:15:33 +0200

    Ajout du fichier de connexion à la base de donnée

---
+++ includes/db.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "admin_archeo-it"; // votre utilisateur MySQL
+$password = "Archeo-It2025"; // votre mot de passe MySQL
+$dbname = "archeo_it"; // votre nom de base
+
+// Création de la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("La connexion a échoué : " . $conn->connect_error);
+}else{
+    echo 'Connexion ok'; // a effacer
+}
+?>
