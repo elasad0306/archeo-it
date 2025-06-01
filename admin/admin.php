8e672b0b01424b74344c5118c57efb161ff9e2d1
El Asad <elasadbennordinehamissi@gmail.com>
2025-06-01 19:51:32 +0200

    Ajout de la page d'accueil du cÃcôté administration

---
+++ admin/admin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include '../includes/head.php'; ?>
+    <meta charset="utf8">
+    <title>Administration</title>
+    <link rel="stylesheet" href="../assets/css/admin.css">
+</head>
+<body>
+
+<div id="administration-page">
+    <div id="menu-band">
+        <ul>
+            <li>Lister les articles</li>
+            <li><a href="add_article.php">Créer des articles</a> </li>
+            <li>Se déconnecter</li>
+        </ul>
+
+
+
+    </div>
+    <h1>Bienvenue sur la page d'admnistration</h1>
+
+</div>
+</body>
+</html>
