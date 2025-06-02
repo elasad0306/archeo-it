8e2b7629a119440a6f1e820353289271152dc339
El Asad <elasadbennordinehamissi@gmail.com>
2025-06-02 23:58:47 +0200

    Ajout duumodal pour générer le mot de passe et ajout du fichier js  script pour l'api

--- register.php
+++ register.php
@@ -3,7 +3,12 @@ include 'includes/db.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-<?php include 'includes/head.php'; ?>
+<head>
+    <?php include 'includes/head.php'; ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+
+</head>
+
 <body>
 <section id="">
     <h1>Archeo-It</h1>
@@ -25,6 +30,10 @@ include 'includes/db.php';
                     <i class="fa-solid fa-user"></i>
                 </span>
             </div>
+            <!--Boutton pour générer un mot de passe!-->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Générer mon mot de passe
+            </button>
             <!--Champ pour le mot de passe !-->
             <div class="control has-icons-left">
                 <input type="password" placeholder="Mot de passe" class="input is-medium">
@@ -40,5 +49,35 @@ include 'includes/db.php';
         </form>
     </div>
 </section>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Générateur de mot de passe</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <label for="numberofcharacter">Nombre de caractère</label>
+                <input type="number" name="number" id="numberofcharacter" class="form-control">
+
+                <label>Difficultés du mot de passe</label>
+                <select class="form-select" id="type-of-password">
+                    <option disabled selected>-- Sélectionner la difficulté du mot de passe --</option>
+                    <option value="alpha" >Alphabétique</option>
+                    <option value="alphaNum">Alphabétique et Numérique</option>
+                    <option value="alphaNumChar">Alphabétique, numérique et caractères spéciaux</option>
+                </select>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger"data-bs-dismiss="modal">Fermé</button>
+                <button type="button" class="btn btn-success" onclick="generatePassword()">Générer </button>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+<script src="assets/js/password.js"></script>
 
 </body>
