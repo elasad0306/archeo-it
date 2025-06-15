00a7ee28d6b4875f600dc118e4da020e694d9bcb
El Asad <elasadbennordinehamissi@gmail.com>
2025-06-15 05:15:33 +0200

    Modification de la page de contact

--- contact.php
+++ contact.php
@@ -2,6 +2,7 @@
   <!DOCTYPE html>
   <html lang="fr">
   <head>
+      <?php include 'includes/head.php'; ?>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Archeo-It - Contact</title>
